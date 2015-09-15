88912e5ee5f967426feccae52940a4d1c3674f10
Wim van Deursen <wimvandeursen@me.com>
2015-09-15 22:35:34 +0200

    Initial Commit

---
+++ tapper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tapper
+//
+//  Created by Wim van Deursen on 15-09-15.
+//  Copyright © 2015 sWimmie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
